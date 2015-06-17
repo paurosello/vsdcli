333e21d0507dedc546d383faf2ef8e452a2dcb59
Christophe Serafin <christophe.serafin@gmail.com>
2015-06-17 16:39:59 -0700

    Improved usage message

--- vsdcli/vsd.py
+++ vsdcli/vsd.py
@@ -10,14 +10,12 @@ class _HelpAction(argparse._HelpAction):
     def __call__(self, parser, namespace, values, option_string=None):
         parser.print_help()
 
-        # retrieve subparsers from parser
         subparsers_actions = [
             action for action in parser._actions
             if isinstance(action, argparse._SubParsersAction)]
-        # there will probably only be one subparser_action,
-        # but better save than sorry
+
         for subparsers_action in subparsers_actions:
-            # get all subparsers and print help
+
             for choice, subparser in subparsers_action.choices.items():
                 print("\n{}:\n{}".format(choice.upper(), '-' * (len(choice) + 1)))
                 print(subparser.format_help())
@@ -44,39 +42,39 @@ def main(argv=sys.argv):
 
     # List Command
     list_parser = subparsers.add_parser('list', description="List all objects", parents=[default_parser])
-    list_parser.add_argument('list', help="Name of the VSD object (See command `objects` to list all objects name)")
-    list_parser.add_argument('--in', dest='parent_infos', nargs=2, help="Specify the parent name and its uuid")
+    list_parser.add_argument('target', help="Name of the VSD object (See command `objects` to list all objects name)")
+    list_parser.add_argument('--in', dest='parent_infos', nargs=2, help="Specify the PARENT_NAME and PARENT_UUID")
     list_parser.add_argument('-f', '--filter', dest='filter', help="Specify a filter predicate")
     list_parser.add_argument('-x', '--fields', dest='fields', help="Specify output fields", nargs='+', type=str)
 
     # Count Command
     list_parser = subparsers.add_parser('count', description="Count all objects", parents=[default_parser])
-    list_parser.add_argument('count', help="Name of the VSD object (See command `objects` to list all objects name)")
+    list_parser.add_argument('target', help="Name of the VSD object (See command `objects` to list all objects name)")
     list_parser.add_argument('--in', dest='parent_infos', nargs=2, help="Specify the parent name and its uuid")
     list_parser.add_argument('-f', '--filter', dest='filter', help="Specify a filter predicate")
     list_parser.add_argument('-x', '--fields', dest='fields', help="Specify output fields", nargs='+', type=str)
 
     # Show Command
     show_parser = subparsers.add_parser('show', description="Show a specific object", parents=[default_parser])
-    show_parser.add_argument('show', help="Name of the object to show (See command `objects` to list all objects name)")
+    show_parser.add_argument('target', help="Name of the object to show (See command `objects` to list all objects name)")
     show_parser.add_argument('-i', '--id', dest='id', help='Identifier of the object to show', required=True)
     show_parser.add_argument('-x', '--fields', dest='fields', help="Specify output fields", nargs='+', type=str)
 
     # Create Command
     create_parser = subparsers.add_parser('create', description="Create a new object", parents=[default_parser])
-    create_parser.add_argument('create', help='Name of the object to create (See command `objects` to list all objects name)')
+    create_parser.add_argument('target', help='Name of the object to create (See command `objects` to list all objects name)')
     create_parser.add_argument('--in', dest='parent_infos', nargs=2, help="Specify the parent name and its uuid")
     create_parser.add_argument('-p', '--params', dest='params', nargs='*', help='List of Key=Value parameters', required=True)
 
     # Update Command
     update_parser = subparsers.add_parser('update', description="Update an existing object", parents=[default_parser])
-    update_parser.add_argument('update', help='Name of the object to update (See command `objects` to list all objects name)')
+    update_parser.add_argument('target', help='Name of the object to update (See command `objects` to list all objects name)')
     update_parser.add_argument('-i', '--id', dest='id', help='Identifier of the object to show', required=True)
     update_parser.add_argument('-p', '--params', dest='params', nargs='*', help='List of Key=Value parameters', required=True)
 
     # Delete Command
     delete_parser = subparsers.add_parser('delete', description="Delete an existing object", parents=[default_parser])
-    delete_parser.add_argument('delete', help='Name of the object to update (See command `objects` to list all objects name)')
+    delete_parser.add_argument('target', help='Name of the object to update (See command `objects` to list all objects name)')
     delete_parser.add_argument('-i', '--id', dest='id', help='Identifier of the object to show', required=True)
 
     # Resources Command
